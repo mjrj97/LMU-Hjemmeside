d9365ff26080dcbf54710ce1a687da8c4c9f4b25
mjrj97 <martin.jensen.1997@hotmail.com>
2021-10-02 11:38:03 +0200

    Made contact form work with one.com

---
+++ send-mail.php
@@ -0,0 +1,34 @@
+<?php
+    // Replace this with your own email address
+    $to="formand@odenselmu.dk";
+
+    // Extract form contents
+    $name = $_POST['realname'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Validate email address
+    function valid_email($str) {
+        return ( ! preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str)) ? FALSE : TRUE;
+    }
+
+    // Return errors if present
+    $errors = "";
+
+    if($name =='') { $errors .= "name,"; }
+    if(valid_email($email)==FALSE) { $errors .= "email,"; }
+    if($message =='') { $errors .= "message,"; }
+
+    // Send email
+    if($errors =='') {
+        header('Location: index.html');
+        $headers =  'From: Kontakt formular <no-reply@odenselmu.dk>'. "\r\n" .
+                    'Reply-To: '.$email.'' . "\r\n" .
+                    'X-Mailer: PHP/' . phpversion();
+        $email_subject = "Besked fra: $email";
+        $message="Name: $name \n\nEmail: $email \n\nMessage: $message";
+        mail($to, $email_subject, $message, $headers);
+    } else {
+        echo $errors;
+    }
+?>
